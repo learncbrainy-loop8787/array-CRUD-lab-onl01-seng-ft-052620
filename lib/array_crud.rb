d81db33f8ca8e5c40d0835665908c5c9ae6de704
korey ko <koreykendrick1@gmail.com>
2020-05-28 18:24:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["killa", 1, 2, 3]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array << 5
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  maru_cat = famous_cats.pop
+
 end
 
 def remove_element_from_start_of_array(array)
